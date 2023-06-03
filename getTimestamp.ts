c5730a3a61732f084cdf9d58c7f949c0dacf8673
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-03 18:32:37 +0200

    Description added to getTimestamp

--- getTimestamp.ts
+++ getTimestamp.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { Timestamp } from 'firebase/firestore';
 
+/** Creates firebase timestamp using dayjs API */
 export const getTimestamp = (date?: dayjs.ConfigType) => {
   return new Timestamp((date ? dayjs(date) : dayjs()).unix(), 0);
 };
