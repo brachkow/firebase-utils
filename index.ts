635b628cefa1dec9f364f9898bc21f3e08d5c480
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-07 17:09:30 +0200

    Add missing AuthService export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { getTimestamp } from './src/utils/getTimestamp';
 export { getId } from './src/utils/getId';
 export { Document, type DocumentData } from './src/types';
+export { AuthService } from './src/services/authService';
